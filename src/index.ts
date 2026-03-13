9e3caf96ea23d972bf085e45fdeeaef91044cd05
Linette Kuhn <linette.kuhn@gmail.com>
2026-03-13 18:39:49 -0400

    Add request logger middleware using pino to log every request automatically

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,16 @@ import testConnection from "./db/test-connection";
 import translateRouter from "./routes/translate.routes";
 import { translationLimiter } from "./middleware/rateLimiter";
 import { env } from "./config/env";
+import { requestLogger } from "./middleware/requestLogger";
 
 const app = express();
 
+// middleware
+app.use(requestLogger);
 app.use(cors());
 app.use(express.json());
 
+// translation router
 app.use("/translate", translationLimiter, translateRouter);
 
 app.get("/", (req, res) => {
